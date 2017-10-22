7a0c9de5443588266e0858ebaadb78e40360459c
krnan9525 <krnan9525@users.noreply.github.com>
2017-10-23 00:28:44 +0100

    HI

---
+++ Assets/boid.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boid : MonoBehaviour {
+
+	public Vector3 force;
+	private steering[] behievers;
+	// Use this for initialization
+	void Start () {
+		behievers = GetComponentsInChildren<steering> ();
+	}
+
+	void FixedUpdate () {
+		foreach (steering b in behievers) {
+			transform.position += b.Calculate();
+		}
+
+	}
+
+
+}
